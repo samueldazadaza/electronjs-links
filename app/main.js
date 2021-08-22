c1b40e5ba12d54f72f77920a0e37e9598b03b3cc
sam <samueldazadaza@gmail.com>
2021-08-22 13:14:37 -0500

    creando constantes para elementos del DOM

--- app/main.js
+++ app/main.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow } = require ('electron');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null; //colocar variable fuera del scope, para mantener app en funcionamiento
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow() //crea una ventana de windows con con cierto tamaño default
+    mainWindow = new BrowserWindow({ //crea una ventana de windows con con cierto tamaño default
+        webPreferences: {
+            nodeIntegration: true, //para poder manejar apis de node a traves de electron
+            contextIsolation : false 
+        }
+    }) 
     mainWindow.loadFile(__dirname + '/index.html' ) //archivo inicial de codigo html
 });
---
+++ app/renderer.js
@@ -0,0 +1,7 @@
+//DOM Elements
+const linkSection = document.querySelector('.links');
+const errormessage = document.querySelector('.error-message');
+const newLinkForm = document.querySelector('.new-link-form');
+const newLinkUrl = document.querySelector('.new-link-url');
+const newLinkButton = document.querySelector('.new-link-button');
+const clearStorageButton = document.querySelector('.clear-storage');
