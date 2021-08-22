e08845dcdbb9327043ad8b317c8bc3d30952dc46
sam <samueldazadaza@gmail.com>
2021-08-22 12:32:11 -0500

    creando ventana inicial de la app :) :)

--- app/main.js
+++ app/main.js
@@ -1,1 +1,6 @@
-console.log("hello world")
+const { app, BrowserWindow } = require ('electron');
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow() //crea una ventana de windows con con cierto tamaño default
+    mainWindow.loadFile(__dirname + '/index.html' ) //archivo inicial de codigo html
+});
